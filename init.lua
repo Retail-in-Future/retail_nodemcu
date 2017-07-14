febccac82e00083cde60ebc067a0854f33348803
tjdai <smile_daitj@yeah.net>
2017-07-14 13:47:32 +0800

    "add wifi cb evt"

--- init.lua
+++ init.lua
@@ -1,30 +1,45 @@
 -- load modules
 config = require("config")
 led_mod = require("led_mod")
-wifi_mod = require("wifi_mod")
 log_mod = require("log_mod")
 gates_mod = require("gates_mod")
 tcp_mod = require("tcp_mod")
 qrcode_mod = require("qrcode_mod")
 
--- wifi
-wifi_mod:start()
-
--- log
 log_mod:init_mod()
 log_mod:print("init log mod")
 
--- gates
-gates_mod:init_mod()
-log_mod:print("start gates")
+function wifi_connected_cb(T)
+    log_mod:print("connect to ap succ")
 
--- tcp server
-tcp_mod:start()
-log_mod:print("start tcp")
+    -- gates
+    gates_mod:init_mod()
+    log_mod:print("start gates")
 
--- qr scanner
-qrcode_mod:start()
-log_mod:print("start qrcode")
+    -- tcp server
+    tcp_mod:start()
+    log_mod:print("start tcp")
+
+    -- qr scanner
+    qrcode_mod:start()
+    log_mod:print("start qrcode")
+end
+
+function wifi_disconnected_cb(T)
+    log_mod:print("disconnect to ap")
+end
+
+-- set wifi evt mon cb
+-- wifi.eventmon.register(wifi.eventmon.STA_CONNECTED, wifi_connected_cb)
+wifi.eventmon.register(wifi.eventmon.STA_DISCONNECTED, wifi_disconnected_cb)
+wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, wifi_connected_cb)
+
+-- connect to wifi
+wifi.setmode(wifi.STATION)
+wifi.sta.config(config.company_wifi_name, config.company_wifi_passwd)
+wifi.sta.autoconnect(1)
+wifi.sta.sethostname("nodemcu")
+wifi.sta.connect()
 
 function test()
     -- body
